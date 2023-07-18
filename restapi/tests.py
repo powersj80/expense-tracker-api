168a51b4064215e43bb41d744b8d3c39cf8f14f1
powersj80 <powersj1980@outlook.com>
2023-07-18 18:46:09 -0400

    Added creation and listing of expenses with API view

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from restapi import models
 from django.urls import reverse
+
+from restapi import models
 
 
 # Create your tests here.
@@ -13,7 +14,6 @@ class TestModels(TestCase):
             category="music",
         )
         inserted_expense = models.Expense.objects.get(pk=expense.id)
-
         self.assertEqual(249.99, inserted_expense.amount)
         self.assertEqual("amazon", inserted_expense.merchant)
         self.assertEqual("anc headphones", inserted_expense.description)
@@ -23,21 +23,17 @@ class TestModels(TestCase):
 class TestViews(TestCase):
     def test_expense_create(self):
         payload = {
-            "amount": 50,
+            "amount": 50.0,
             "merchant": "AT&T",
             "description": "cell phone subscription",
             "category": "utilities",
         }
-
         res = self.client.post(
             reverse("restapi:expense-list-create"), payload, format="json"
         )
-
         self.assertEqual(201, res.status_code)
-
         json_res = res.json()
-
-        self.assertEqual(str(payload["amount"]), json_res["amount"])
+        self.assertEqual(payload["amount"], json_res["amount"])
         self.assertEqual(payload["merchant"], json_res["merchant"])
         self.assertEqual(payload["description"], json_res["description"])
         self.assertEqual(payload["category"], json_res["category"])
@@ -45,12 +41,21 @@ class TestViews(TestCase):
 
     def test_expense_list(self):
         res = self.client.get(reverse("restapi:expense-list-create"), format="json")
-
         self.assertEqual(200, res.status_code)
-
         json_res = res.json()
-
         self.assertIsInstance(json_res, list)
-
         expenses = models.Expense.objects.all()
         self.assertEqual(len(expenses), len(json_res))
+
+    def test_expense_create_required_fields_missing(self):
+        payload = {
+            "merchant": "AT&T",
+            "description": "cell phone subscription",
+            "category": "utilities",
+        }
+        res = self.client.post(
+            reverse("restapi:expense-list-create"), payload, format="json"
+        )
+        self.assertEqual(400, res.status_code)
+        json_res = res.json()
+        print(json_res)
